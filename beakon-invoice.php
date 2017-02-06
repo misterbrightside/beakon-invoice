f7166eaf216b6b01619517c4da5a919464aa82ec
John Brennan <john.brennan55@mail.dcu.ie>
2017-02-06 17:16:22 +0000

    initial commit

---
+++ beakon-invoice.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Beakon Invoice
+   Plugin URI: http://beakon.ie/
+   Description: Beakon invoices
+   Version: 0.1
+   Author: John Brennan
+   Author URI: http://sentogue.com
+   License: GPL2
+   */
+?>
