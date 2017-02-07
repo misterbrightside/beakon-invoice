0349ae8f3164ff31eb0e61648784a161f3f03a22
John Brennan <john.brennan55@mail.dcu.ie>
2017-02-07 16:10:28 +0000

    saving + persistence to single invoice.

--- admin/invoice-fields.php
+++ admin/invoice-fields.php
@@ -11,7 +11,7 @@ function bijb_add_user_info_metabox() {
 	);
 }
 
-function bijb_get_input_tag($id, $title, $input) {
+function bijb_get_input_tag( $id, $title, $input ) {
 	return "
 		<div class='meta-row'>
 			<div class='meta-th'>
@@ -22,18 +22,99 @@ function bijb_get_input_tag($id, $title, $input) {
 	";
 }
 
-function bijb_get_text_input($id) {
-	return "<input type='text' name='$id' id='$id' value='' />";
+function bijb_get_text_input( $id, $value ) {
+	return "<input type='text' name='$id' id='$id' value='$value' />";
 }
 
-function bijb_user_template() {
+function bijb_get_value( $meta, $id ) {
+	if ( ! empty( $meta[ $id ] ) ) {
+		return esc_attr( $meta[ $id ][0] );
+	} else {
+		return '';
+	}
+}
+
+function bijb_get_first_name( $meta ) {
+	return bijb_get_input_tag(
+		'first-name-id',
+		'First Name',
+		bijb_get_text_input(
+			'first-name-id',
+			bijb_get_value( $meta, 'first-name-id' )
+		)
+	);
+}
+
+function bijb_get_surname( $meta ) {
+	return bijb_get_input_tag(
+		'surname-id',
+		'Surname',
+		bijb_get_text_input(
+			'surname-id',
+			bijb_get_value( $meta, 'surname-id' )
+		)
+	);
+}
+
+function bijb_get_address_line( $meta, $number ) {
+	return bijb_get_input_tag(
+		"address-line-$number-id",
+		"Address Line $number",
+		bijb_get_text_input(
+			"address-line-$number-id",
+			bijb_get_value( $meta, "address-line-$number-id" )
+		)
+	);
+}
+
+function bijb_get_county( $meta ) {
+	return bijb_get_input_tag(
+		'county-id',
+		'County',
+		bijb_get_text_input(
+			'county-id',
+			bijb_get_value( $meta, 'county-id' )
+		)
+	);
+}
+
+function bijb_user_template( $invoice ) {
+	wp_nonce_field( basename( __FILE__ ), 'dijb_invoices_nonce' );
+	$bijb_stored_meta = get_post_meta( $invoice -> ID );
 	echo '<div>'
-		. bijb_get_input_tag('first-name-id', 'First Name', bijb_get_text_input( 'first-name-id' ))
-		. bijb_get_input_tag('surname-id', 'Surname', bijb_get_text_input( 'surname-id' ))
-		. bijb_get_input_tag('address-line-1-id', 'Address Line 1', bijb_get_text_input( 'address-line-1-id' ))
-		. bijb_get_input_tag('address-line-2-id', 'Address Line 2', bijb_get_text_input( 'address-line-2-id' ))
-		. bijb_get_input_tag('address-line-3-id', 'Address Line 3', bijb_get_text_input( 'address-line-3-id' ))
+		. bijb_get_first_name( $bijb_stored_meta )
+		. bijb_get_surname( $bijb_stored_meta )
+		. bijb_get_address_line( $bijb_stored_meta, '1' )
+		. bijb_get_address_line( $bijb_stored_meta, '2' )
+		. bijb_get_address_line( $bijb_stored_meta, '3' )
+		. bijb_get_county( $bijb_stored_meta )
 		. '</div>';
 }
 
+function bijb_is_valid_nonce() {
+	return ( isset( $_POST['dijb_invoices_nonce'] ) && wp_verify_nonce( $_POST['dijb_invoices_nonce'], basename( __FILE__ ) ) ) ? 'true' : 'false';
+}
+
+function bijb_save_field( $invoice_id, $id ) {
+	if ( isset( $_POST[ $id ] ) ) {
+		update_post_meta( $invoice_id, $id, sanitize_text_field( $_POST[ $id ] ) );
+	}
+}
+
+function bijb_save_user_metadata( $invoice_id ) {
+	$is_autosave = wp_is_post_autosave( $invoice_id );
+	$is_revision = wp_is_post_revision( $invoice_id );
+	if ( $is_autosave || $is_revision || ! bijb_is_valid_nonce() ) {
+		return;
+	}
+
+	bijb_save_field( $invoice_id, 'first-name-id' );
+	bijb_save_field( $invoice_id, 'surname-id' );
+	bijb_save_field( $invoice_id, 'address-line-1-id' );
+	bijb_save_field( $invoice_id, 'address-line-2-id' );
+	bijb_save_field( $invoice_id, 'address-line-3-id' );
+	bijb_save_field( $invoice_id, 'county-id' );
+}
+
+add_action( 'save_post', 'bijb_save_user_metadata' );
 add_action( 'add_meta_boxes', 'bijb_add_user_info_metabox' );
