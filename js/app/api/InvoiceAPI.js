0abcedde924346564afab4306e9bc7a6a69ba9c7
John Brennan <john.brennan55@mail.dcu.ie>
2017-02-09 12:32:31 +0000

    format changes

--- js/app/api/InvoiceAPI.js
+++ js/app/api/InvoiceAPI.js
@@ -1,17 +1,17 @@
 export default class InvoiceAPI {
 
-  static getForm (invoiceId, surname) {
-    const form = new FormData()
-    form.append('invoiceId', escape(invoiceId.trim()))
-    form.append('surname', escape(surname.trim()))
-    return form
+  static getForm(invoiceId, surname) {
+    const form = new FormData();
+    form.append('invoiceId', escape(invoiceId.trim()));
+    form.append('surname', escape(surname.trim()));
+    return form;
   }
 
-  static checkWhetherInvoiceExists (invoiceId, surname) {
+  static checkWhetherInvoiceExists(invoiceId, surname) {
     return fetch('/wp-json/beakon-invoices/v1/invoice-exists', {
       method: 'POST',
-      body: this.getForm(invoiceId, surname)
+      body: this.getForm(invoiceId, surname),
     }).then(response => response.json())
-			.then(json => JSON.parse(json))
+      .then(json => JSON.parse(json));
   }
 }
--- js/app/index.js
+++ js/app/index.js
@@ -1,138 +1,41 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import InvoiceLoginField from './InvoiceLoginField'
-import InvoiceAPI from './api/InvoiceAPI'
-import style from './login-page.css'
-import { escape } from 'lodash'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import InvoiceAPI from './api/InvoiceAPI';
 
-const CheckInvoiceButton = () => (
-  <div className={style.findInvoiceButtonContainer}>
-    <button type="submit">
-			Find my invoice ➔
-		</button>
-  </div>
-)
+class PayInvoicesApplication extends Component {
+  constructor() {
+    super();
+    
+  }
 
-const InputFieldError = ({ label }) => (
-	<div className={style.inputFieldErrorMessage}>
-		{ label }
-	</div>
-)
-
-const InvoiceAPISearchIndicator = ({ invoiceErrorMessage, displayMessage, isSearchingForInvoice }) => {
-	if (isSearchingForInvoice) {
-		return (
-			<div>Loading...</div>
-		)
-	} else if (displayMessage && invoiceErrorMessage) {
-		return (
-			<div>{ invoiceErrorMessage }</div>
-		)
-	} else return null;
-}
-
-class InvoiceLogin extends Component {
-  constructor () {
-    super()
-    this.onSubmitForm = this.onSubmitForm.bind(this)
-    this.updateFieldValue = this.updateFieldValue.bind(this)
-    this.state = {
-			isSearchingForInvoice: false,
-			invoiceErrorMessage: '',
-			displayMessage: false,
-      invoiceId: this.getInitialInputState(),
-      surname: this.getInitialInputState(),
-			invoice: null
+  onSubmitForm(event) {
+    const { invoiceId, surname } = this.state;
+    event.preventDefault();
+    if (invoiceId.touched && surname.touched) {
+      this.setState({ isSearchingForInvoice: true });
+      InvoiceAPI.checkWhetherInvoiceExists(invoiceId.value, surname.value)
+        .then(({ invoiceExists, invoice }) => this.setState({
+          isSearchingForInvoice: false,
+          displayMessage: !invoiceExists,
+          invoiceErrorMessage: !invoiceExists ? 'No invoice found!' : '',
+          invoice,
+        }))
+        .catch((error) => {
+          console.error(error);
+          return this.setState({
+            isSearchingForInvoice: false,
+            invoiceErrorMessage: 'Error checking the database, please try again later.',
+            displayMessage: true,
+          });
+        });
+    } else {
+      this.setState({
+        isSearchingForInvoice: false,
+        displayMessage: true,
+        invoiceErrorMessage: 'Fill in all info!',
+      });
     }
   }
-
-	getInitialInputState() {
-		return ({
-			value: '', valid: false, touched: false
-		})
-	}
-
-  updateFieldValue = (id) => (event) => {
-    event.preventDefault()
-    const { value } = event.target
-		return this.setState({
-			[id]: {
-				value,
-				valid: value.trim().length > 0,
-				touched: true
-			}
-		})
-  }
-
-  onSubmitForm (event) {
-		const { invoiceId, surname } = this.state
-    event.preventDefault()
-		if (invoiceId.touched && surname.touched) {
-			this.setState({ isSearchingForInvoice: true })
-			InvoiceAPI.checkWhetherInvoiceExists(invoiceId.value, surname.value)
-				.then(({ invoiceExists, invoice }) => this.setState({
-					isSearchingForInvoice: false,
-					displayMessage: !invoiceExists,
-					invoiceErrorMessage: !invoiceExists ? 'No invoice found!' : '',
-					invoice
-				}))
-				.catch(error => {
-					console.error(error)
-					return this.setState({
-						isSearchingForInvoice: false,
-						invoiceErrorMessage: 'Error checking the database, please try again later.',
-						displayMessage: true
-					})
-				})
-		} else {
-			this.setState({
-				isSearchingForInvoice: false,
-				displayMessage: true,
-				invoiceErrorMessage: 'Fill in all info!'
-			})
-		}
-  }
-
-
-
-	isValid = (field) => {
-		return this.state[field].valid || !this.state[field].touched
-	}
-
-  render () {
-    const { invoiceId, surname, isSearchingForInvoice, displayMessage, invoiceErrorMessage } = this.state
-    return (
-      <div className={style.invoicesLogin}>
-        <form
-					className={style.invoicesContainer}
-					onSubmit={this.onSubmitForm}
-				>
-          <h1 className={style.invoicesLoginHeader}>Pay a Bill</h1>
-          <InvoiceLoginField
-            label={'Invoice Number'}
-            value={invoiceId.value}
-            onUpdateInput={this.updateFieldValue('invoiceId')}
-						errorMessage={<InputFieldError label={'You must enter a valid invoice reference.'} />}
-						isValid={this.isValid('invoiceId')}
-					/>
-          <InvoiceLoginField
-            label={'Surname'}
-            value={surname.value}
-            onUpdateInput={this.updateFieldValue('surname')}
-						errorMessage={<InputFieldError label={'You must enter a valid surname.'} />}
-						isValid={this.isValid('surname')}
-					/>
-          <CheckInvoiceButton />
-					{ displayMessage ?
-						<InvoiceAPISearchIndicator
-							invoiceErrorMessage={invoiceErrorMessage}
-							displayMessage={displayMessage}
-							isSearchingForInvoice={isSearchingForInvoice}
-						/> : null }
-        </form>
-      </div>
-    )
-  }
 }
 
-ReactDOM.render(<InvoiceLogin />, document.getElementById('invoices-app'))
+ReactDOM.render(<PayInvoicesApplication />, document.getElementById('invoices-app'));
