b0de21d253118f5f63b8e8f812e62ff67aec86d6
John Brennan <john.brennan55@mail.dcu.ie>
2017-02-09 13:33:28 +0000

    refactoring state to top level component

--- js/app/InvoiceLogin.js
+++ js/app/InvoiceLogin.js
@@ -7,7 +7,7 @@ import { escape } from 'lodash'
 
 const CheckInvoiceButton = () => (
   <div className={ style.findInvoiceButtonContainer }>
-    <button type="submit">
+    <button type='submit'>
       Find my invoice ➔
     </button>
   </div>
@@ -25,73 +25,63 @@ const InvoiceAPISearchIndicator = ({invoiceErrorMessage, displayMessage, isSearc
       <div>
         Loading...
       </div>
-    );
+      );
   } else if (displayMessage && invoiceErrorMessage) {
     return (
       <div>
         { invoiceErrorMessage }
       </div>
-    );
+      );
   } else return null;
 };
 
 class InvoiceLogin extends Component {
   constructor() {
-    super()
-    this.onSubmitForm = this.onSubmitForm.bind(this)
-    this.updateFieldValue = this.updateFieldValue.bind(this)
+    super();
   }
-
-  getInitialInputState() {
-    return ({
-      value: '',
-      valid: false,
-      touched: false
-    })
-  }
-
-  updateFieldValue = (id) => (event) => {
-    event.preventDefault()
-    const {value} = event.target
-    return this.setState({
-      [id]: {
-        value,
-        valid: value.trim().length > 0,
-        touched: true
-      }
-    })
-  }
-
-
 
   isValid = (field) => {
-    return this.state[field].valid || !this.state[field].touched
+    return this.props[field].valid || !this.props[field].touched
   }
 
   render() {
-    const {invoiceId, surname, isSearchingForInvoice, displayMessage, invoiceErrorMessage} = this.props;
+    const {
+      invoiceId, 
+      surname,
+      isSearchingForInvoice,
+      displayMessage,
+      invoiceErrorMessage, 
+      onSubmitForm,
+      updateFieldValue
+    } = this.props;
+    
     return (
       <div className={ style.invoicesLogin }>
-        <form className={ style.invoicesContainer } onSubmit={ this.onSubmitForm }>
+        <form className={ style.invoicesContainer } onSubmit={ onSubmitForm }>
           <h1 className={ style.invoicesLoginHeader }>Pay a Bill</h1>
           <InvoiceLoginField label={ 'Invoice Number' }
             value={ invoiceId.value }
-            onUpdateInput={ this.updateFieldValue('invoiceId') }
+            onUpdateInput={ updateFieldValue('invoiceId') }
             errorMessage={ <InputFieldError label={ 'You must enter a valid invoice reference.' } /> }
             isValid={ this.isValid('invoiceId') }
           />
           <InvoiceLoginField label={ 'Surname' }
             value={ surname.value }
-            onUpdateInput={ this.updateFieldValue('surname') }
+            onUpdateInput={ updateFieldValue('surname') }
             errorMessage={ <InputFieldError label={ 'You must enter a valid surname.' } /> }
             isValid={ this.isValid('surname') }
           />
           <CheckInvoiceButton />
           { displayMessage ?
-            <InvoiceAPISearchIndicator invoiceErrorMessage={ invoiceErrorMessage } displayMessage={ displayMessage } isSearchingForInvoice={ isSearchingForInvoice } /> : null }
+            <InvoiceAPISearchIndicator
+              invoiceErrorMessage={ invoiceErrorMessage }
+              displayMessage={ displayMessage }
+              isSearchingForInvoice={ isSearchingForInvoice }
+            /> : null 
+          }
         </form>
       </div>
-    );
+      );
   }
 }
 
--- js/app/index.js
+++ js/app/index.js
@@ -1,40 +1,123 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import InvoiceLogin from './InvoiceLogin';
 import InvoiceAPI from './api/InvoiceAPI';
 
+
 class PayInvoicesApplication extends Component {
+
   constructor() {
     super();
-    
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+    this.updateFieldValue = this.updateFieldValue.bind(this);
+    this.state = {
+      loginForm: {
+        isSearchingForInvoice: false,
+        invoiceErrorMessage: '',
+        displayMessage: false,
+        invoiceId: this.getInitialInputState(),
+        surname: this.getInitialInputState(),
+        invoice: null
+      },
+    };
   }
 
-  onSubmitForm(event) {
-    const { invoiceId, surname } = this.state;
-    event.preventDefault();
-    if (invoiceId.touched && surname.touched) {
-      this.setState({ isSearchingForInvoice: true });
-      InvoiceAPI.checkWhetherInvoiceExists(invoiceId.value, surname.value)
-        .then(({ invoiceExists, invoice }) => this.setState({
+  getInitialInputState() {
+    return ({
+      value: '',
+      valid: false,
+      touched: false
+    });
+  }
+
+  setStateAfterCheckingWhetherInvoiceExists = ({ invoiceExists, invoice }) => {
+    return this.setState((previousState) => ({ 
+        loginForm: Object.assign({}, previousState.loginForm, {
           isSearchingForInvoice: false,
           displayMessage: !invoiceExists,
           invoiceErrorMessage: !invoiceExists ? 'No invoice found!' : '',
           invoice,
-        }))
-        .catch((error) => {
-          console.error(error);
-          return this.setState({
-            isSearchingForInvoice: false,
-            invoiceErrorMessage: 'Error checking the database, please try again later.',
-            displayMessage: true,
-          });
-        });
-    } else {
-      this.setState({
+        })
+      })
+    );
+  }
+
+  setStateAfterCheckingInvoiceExistsFailed = (error) => {
+    console.error(error);
+    return this.setState(() => ({
+      loginForm: Object.assign({}, previousState.loginForm, {
+        isSearchingForInvoice: false,
+        invoiceErrorMessage: 'Error checking the database, please try again later.',
+        displayMessage: true,
+      })
+    }));
+  }
+
+  setLoadingState() {
+    return this.setState((previousState) => ({
+      loginForm: Object.assign({}, previousState.loginForm, {
+        isSearchingForInvoice: true,
+      })
+    }));
+  }
+
+  tellUserToFillInAllInfo() {
+    return this.setState((previousState) => ({
+      loginForm: Object.assign({}, previousState.loginForm, {
         isSearchingForInvoice: false,
         displayMessage: true,
         invoiceErrorMessage: 'Fill in all info!',
-      });
+      })
+    }))
+  }
+
+  fieldValid(field) {
+    return field.touched && field.valid;
+  }
+
+  formValid(invoiceId, surname) {
+    return this.fieldValid(invoiceId) && this.fieldValid(surname);
+  }
+
+  onSubmitForm(event) {
+    const {invoiceId, surname} = this.state.loginForm;
+    event.preventDefault();
+    if (this.formValid(invoiceId, surname)) {
+      this.setLoadingState();
+      InvoiceAPI.checkWhetherInvoiceExists(invoiceId.value, surname.value)
+        .then(this.setStateAfterCheckingWhetherInvoiceExists)
+        .catch(this.setStateAfterCheckingInvoiceExistsFailed);
+    } else {
+      this.tellUserToFillInAllInfo();
     }
+  }
+
+  setNewValue(id, value) {
+    return this.setState((previousState) => ({
+      loginForm: Object.assign({}, previousState.loginForm, {
+        [id]: {
+          value,
+          valid: value.trim().length > 0,
+          touched: true,
+        }})}
+      )
+    );
+  }
+
+  updateFieldValue = (id) => (event) => {
+    event.preventDefault();
+    const { value } = event.target;
+    return this.setNewValue(id, value);
+  }
+
+  render() {
+    return (
+      <InvoiceLogin 
+        { ... this.state.loginForm }
+        updateFieldValue={this.updateFieldValue}
+        onSubmitForm={this.onSubmitForm}
+      />
+    );
   }
 }
 
